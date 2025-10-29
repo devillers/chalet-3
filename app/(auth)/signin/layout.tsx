c8ab0e88cc8185b2619314b5728a4c858ff4872d
devillers <davidevillers@gmail.com>
2025-10-29 01:00:08 +0100

    f

--- app/(auth)/signin/layout.tsx
+++ app/(auth)/signin/layout.tsx
@@ -5,9 +5,6 @@ export default function SigninLayout({
 }: {
   children: ReactNode;
 }) {
-  return (
-    <div className="min-h-screen flex flex-col bg-slate-50 dark:bg-slate-900"></div>
+  return children;
   
-  children;
-
 }
